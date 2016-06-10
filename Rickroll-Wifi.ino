1856d97436cc32f83695e8650e6b8f25cb8377ba
Arda Özkal <ardaozkal@users.noreply.github.com>
2016-06-10 06:26:34 +0300

    Adding in review from CodeReview.SE
    Attribution: The code samples by Quuxplusone ( http://codereview.stackexchange.com/a/131589/95287 ) were used and they are licensed with CC-BY-SA

--- Rickroll-Wifi.ino
+++ Rickroll-Wifi.ino
@@ -1,4 +1,5 @@
 #include <ESP8266WiFi.h>
+#define DIM(arr) (sizeof (arr) / sizeof (arr)[0])
 
 const char* ssids[] = {"Never gonna give you up","Never gonna let you down","Never gonna run around","Never gonna make you cry","Never gonna say goodbye","Never gonna tell a lie"};
 const char* pass = "pass_goes_here";
@@ -14,11 +15,7 @@ void setup() {
     WiFi.softAP(ssid, pass);
     delay(5000);
     WiFi.softAPdisconnect(false);
-    currentssidno = currentssidno + 1;
-    if (currentssidno == 6) //please change this count if you change the amount of ssids
-    {
-      currentssidno = 0;
-    }
+    currentssidno = (currentssidno + 1) % DIM(ssids);
   }
 }
 
